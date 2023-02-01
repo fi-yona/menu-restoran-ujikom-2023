8fd122f71ca6d134694a153e31d395aeae26c307
fi-yona <fionaavila15@gmail.com>
2023-02-02 04:00:52 +0700

    Add Info

--- app/src/main/java/com/fionaavila/pemesananmenu/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/fionaavila/pemesananmenu/ui/slideshow/SlideshowFragment.java
@@ -4,17 +4,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.fionaavila.pemesananmenu.R;
 import com.fionaavila.pemesananmenu.databinding.FragmentSlideshowBinding;
 
 public class SlideshowFragment extends Fragment {
 
     private FragmentSlideshowBinding binding;
+
+    private Button button_keluar;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,8 +28,6 @@ public class SlideshowFragment extends Fragment {
         binding = FragmentSlideshowBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textSlideshow;
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
